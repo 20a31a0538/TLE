9f219007d863cacf1517bec4ec43a9623ee584c0
20a31a0538 <110081197+20a31a0538@users.noreply.github.com>
2023-08-06 22:06:31 +0530

    Add files via upload

---
+++ TLE/Week 1/basics.cpp
@@ -0,0 +1,67 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //efficient & speed
+    //largest community
+    //in built DSA(STL) ---> sets, maps(ordered, unordered)
+    cout<<"Hello World";
+    //constants --> int, decimal, char, string literal
+    //char --> 'a', 'b',...
+    //string literal(value)--> collection of character
+    //string --> datatype
+    //OUTPUT --> cout operator
+    cout<<endl;
+   /* cout << "prince" <<" "<< "pavan"; */
+    //ARTHIMETIC OPERATORS --> *,/,+,-, %
+    cout << 10/3.0;
+    //VARIABLE  ---> container --> stores specific kind of value
+    //types styling --> snake, camel style
+    cout<<endl;
+    int p = 10;
+    cout << p;
+    cout <<endl;
+    
+    //already taken varible is not present within same scope
+    
+    //DATATYPES ---> primitive, derived
+   // bool --> true(anything except 0 ---> )
+   //derived --> string, vector, map, set, priority_queue
+   //unary ---> +, -, ++, ---
+   //++x --> inc first then do
+   //x++  --> do then inc
+  /* int x= 10;
+   
+   cout << ++x; */
+   //INPUT
+  /* int x, y;
+   cin >> x >> y;
+   cout << x << y; */
+  /* int x=5;
+   x=x++;
+   cout<<x++; */
+   /*float a;
+   float b;
+   cin>>a>>b;
+   cout<<a+b << " " <<a*b <<" " <<(float)a/b; */
+   //CONDITIONAL & RELATIONAL
+  /* int x=5, y=10;
+   cout<<(x<5)<<(y>4); */
+   //logical operator --> &&, ||, !(exp)
+    
+   
+    
+    
+
+    return 0;
+}
